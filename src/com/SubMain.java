5ba27d84d61fd648dabfa6030a658b9d49c5a319
likemindg <60978968+likemindg@users.noreply.github.com>
2022-08-29 16:57:10 +0900

    Update SubMain.java

--- src/com/SubMain.java
+++ src/com/SubMain.java
@@ -4,16 +4,7 @@ public class SubMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 하나
-		// second...
-		// 세번째...
-		// 추가했어..
-		// 네번째.....
-
-		// 고래?
-		// 셋.
-
-		// 끝인가?
+		// 다시 시작이야..
 
 	}
 
